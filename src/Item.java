10fe189c1c7687c3ab7abcc88d9392a04c4cc797
OZIIJIN <154957716+OZIIJIN@users.noreply.github.com>
2024-01-09 11:18:47 +0900

    Delete src directory

--- src/Item.java
+++
@@ -1,7 +0,0 @@
-public class Item extends Menu{
-    double price;
-    public Item (String name, double price, String discription){
-        super(name, discription);
-        this.price = price;
-    }
-}
